2b53ca6502406b9033310964beda4b489e131d69
Olusola Samuel <olusola.samuel.oluwatobi@gmail.com>
2019-04-03 12:38:08 +0100

    fetched data for all orders page

---
+++ js/all_orders.js
@@ -0,0 +1,46 @@
+const user_id = localStorage.getItem("user_id");
+const token = localStorage.getItem("token");
+const username = localStorage.getItem("username");
+
+const url = `https://sendit-olusola.herokuapp.com/api/v1/users/${user_id}/parcels`;
+
+function getData() {
+// clearing up the error message 
+// document.getElementById('error_message').innerHTML = "";
+
+const result = fetch(url, {
+    credentials: 'same-origin', // 'include', default: 'omit'
+    method: 'GET', // 'GET', 'PUT', 'DELETE', etc.
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }),
+  })
+  .then(response => response.json())
+  .then(res => {
+    if(res.status == 401){
+      window.location.href = "login.html"
+    }
+
+
+    let parcelOrders = res.parcelOrders;
+    let dataObject = parcelOrders.map(resObj => resObj.data)
+    let parcelIds = dataObject.map(a => a[0].id)
+
+    parcelIds.forEach(id => {
+      // create a list element
+      let node = document.createElement("h3");
+      let parcel_link = `https://sendit-olusola.herokuapp.com/api/v1/parcels/${id}`;
+      node.innerHTML += `<a href="order_details.html?orderId=${id}">Order ${id} </a>`;
+      document.getElementById('parcel_list').appendChild(node)
+    })
+
+
+  })
+  .catch(e => console.log(e))
+
+  console.log(result)
+}
+
+
+
