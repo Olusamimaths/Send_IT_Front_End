95627b1c4796ff6e2a7a692d3c6262847464ee3b
Olusola Samuel <olusola.samuel.oluwatobi@gmail.com>
2019-04-06 09:03:17 +0100

    Fixing merge conflict

--- js/helpers/helper.profile.js
+++ js/helpers/helper.profile.js
@@ -5,22 +5,13 @@ const isAdmin = localStorage.getItem("isAdmin");
 
 function getDeliveredOrders(dataObject){
     let statusArr = dataObject.map(a => a[0].status)
-<<<<<<< HEAD
-    const delivered = statusArr.filter(status => status.toLowerCase() === "delivered").length
-    const transit = statusArr.filter(status => status.toLowerCase() === "transit").length
-=======
     const delivered = statusArr.filter(status => status.toLowerCase() == "delivered").length
     const transit = statusArr.filter(status => status.toLowerCase() == "transit").length
->>>>>>> 1d316b1921942d1769839bfa3fbeda68db19c8fb
     
     if(delivered){
         document.getElementById("delivered").innerHTML = delivered
     }
     if(transit){
-<<<<<<< HEAD
-        document.getElementById("delivered").innerHTML = transit
-=======
         document.getElementById("transit").innerHTML = transit
->>>>>>> 1d316b1921942d1769839bfa3fbeda68db19c8fb
     }
 }
